17c40a9176c2393b34d50ca71bd18b30cf8b0600
Marc Lynch <lynchmarc2@gmail.com>
2022-01-10 15:52:38 -0600

    chore: gh action

--- packages/cli/test/commands/tsc.test.ts
+++ packages/cli/test/commands/tsc.test.ts
@@ -2,38 +2,34 @@ import { test } from "@oclif/test";
 import { expect, assert } from "chai";
 import execa = require("execa");
 import { existsSync, readJSONSync, remove } from "fs-extra";
-import { join } from "path";
-import { resolve } from "path";
+import { join, resolve } from "path";
+import { setGracefulCleanup } from "tmp";
 
 import type { Report } from "@rehearsal/reporter";
 
-import { TSC } from "../../src";
-import { YARN_PATH } from "../test-helpers";
-import { git } from "../../src/utils";
+export const { VOLTA_HOME } = process.env as { VOLTA_HOME: string };
+export const YARN_PATH = resolve(VOLTA_HOME, "bin/yarn");
+
+import { TSC, git, getPathToBinary } from "../../src";
 
 const FIXTURE_APP_PATH = resolve(__dirname, "../fixtures/app");
 const RESULTS_FILEPATH = join(FIXTURE_APP_PATH, ".rehearsal.json");
 // we need an older version of typescript that'll flag errors
 let TSC_VERSION = "4.2.4";
 
-const beforeEachDefault = async () => {
+describe("ts:command against fixture", async () => {
+  const tscBinary = await getPathToBinary(YARN_PATH, "tsc");
+  const { stdout } = await execa(tscBinary, ["--version"]);
+  // stdout "Version N.N.N" split at the space
+  TSC_VERSION = stdout.split(" ")[1];
+
   await execa(YARN_PATH, [
     "add",
     "-D",
     `typescript@${TSC_VERSION}`,
     "--ignore-scripts"
   ]);
-};
 
-const afterEachCleanup = async () => {
-  await remove(join(FIXTURE_APP_PATH, ".rehearsal.json"));
-  await git(
-    ["restore", "package.json", "../../yarn.lock", FIXTURE_APP_PATH],
-    process.cwd()
-  );
-};
-
-describe("ts:command against fixture", async () => {
   test.stdout().it("WITH autofix", async (ctx) => {
     await TSC.run([
       "--src_dir",
@@ -74,68 +70,18 @@ describe("ts:command against fixture", async () => {
     assert.equal(firstFileReportError.isAutofixed, true);
     assert.equal(firstFileReportError.stringLocation.end, 326);
     assert.equal(firstFileReportError.stringLocation.start, 236);
-
-    test.stdout().it("NO autofix", async (ctx) => {
-      await TSC.run([
-        "--src_dir",
-        FIXTURE_APP_PATH,
-        "--dry_run",
-        "--is_test",
-        "--report_output",
-        FIXTURE_APP_PATH
-      ]);
-
-      expect(ctx.stdout).to.contain(`Autofix not enabled`);
-    });
-  });
-})
-  .beforeEach(async () => {
-    await beforeEachDefault();
-  })
-  .afterEach(async () => {
-    // cleanup and reset
-    await afterEachCleanup();
   });
 
-describe("ts:command tsc version check", async () => {
-  test.stderr().it(`on typescript invalid tsc_version`, async () => {
-    try {
-      await TSC.run(["--tsc_version", ""]);
-    } catch (error) {
-      expect(`${error}`).to.contain(
-        `The tsc_version specified is an invalid string. Please specify a valid version as n.n.n`
-      );
-    }
-
-    try {
-      await TSC.run(["--tsc_version", "0"]);
-    } catch (error) {
-      expect(`${error}`).to.contain(
-        `The tsc_version specified is an invalid string. Please specify a valid version as n.n.n`
-      );
-    }
-  });
-
-  test.stdout().it(`on typescript version already tested`, async (ctx) => {
-    // this will test the version already installed
-    await TSC.run([
-      "--src_dir",
-      FIXTURE_APP_PATH,
-      "--tsc_version",
-      TSC_VERSION,
-      "--dry_run",
-      "--is_test"
-    ]);
-
-    expect(ctx.stdout).to.contain(
-      `This application is already on the latest version of TypeScript@${TSC_VERSION}`
-    );
-  });
-})
-  .beforeEach(async () => {
-    await beforeEachDefault();
-  })
-  .afterEach(async () => {
-    // cleanup and reset
-    await afterEachCleanup();
-  });
+  await remove(join(FIXTURE_APP_PATH, ".rehearsal.json"));
+  await git(
+    ["restore", "package.json", "../../yarn.lock", FIXTURE_APP_PATH],
+    process.cwd()
+  );
+  await execa(YARN_PATH, [
+    "add",
+    "-D",
+    `typescript@~${TSC_VERSION}`,
+    "--ignore-scripts"
+  ]);
+  setGracefulCleanup();
+});
